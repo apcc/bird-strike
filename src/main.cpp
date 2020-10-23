0666631272fd85e6de6c2482155fc8d7e97e3ec6
subaru <s@su8ru.dev>
2020-10-23 23:42:38 +0900

    feat: add window and scene size config

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,11 @@
 void Main() {
   FontAsset::Register(U"TitleFont", 60, Typeface::Heavy);
 
+  Scene::Resize(Size(1920, 1080));
+  Window::Resize(Size(1920, 1080), WindowResizeOption::KeepSceneSize);
+  Window::SetStyle(WindowStyle::Sizable);
+  Window::SetTitle(U"Bird Strike!");
+
   App manager;
 
   registerScenes(manager);
